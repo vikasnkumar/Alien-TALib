f55b20ac54ca3807953b83d8645e2ae359c9f3b7
Vikas N Kumar <vikas@cpan.org>
2013-12-17 17:05:44 -0500

    Adding a dummy build system

---
+++ lib/Alien/TALib.pm
@@ -0,0 +1,13 @@
+package Alien::TALib;
+use strict;
+use warnings;
+
+BEGIN {
+    our $VERSION = 0.01;
+}
+
+__END__
+#### COPYRIGHT: Vikas N Kumar. All Rights Reserved
+#### AUTHOR: Vikas N Kumar <vikas@cpan.org>
+#### DATE: 17th Dec 2013
+#### LICENSE: Refer LICENSE file.
